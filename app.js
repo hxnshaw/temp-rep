fdd3f8452d7469f012396a91d216a8c89d1a702c
hxnshaw <eghosasere07@gmail.com>
2022-06-11 13:12:49 +0100

    test commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// const amount = 12;
+
+// if (amount < 10) {
+//   console.log("small number");
+// } else {
+//   console.log("large number");
+// }
+
+// console.log(`First Tut`);
+
+const os = require("os");
+const user = os.userInfo();
+console.log(user);
+
+console.log(`Uptime is ${os.uptime()} seconds`);
